ab68aff829c307e4e97de59dd69348b758c77a93
linguowei <1368033036@qq.com>
2017-08-17 16:40:15 +0800

    update route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,16 +7,16 @@ const Models = require('../models')
 const resolve = file => path.resolve(__dirname, file)
 const router = new Router()
 
+// 定义成功的返回模板
 const successState = {
 	msg: 'success',
 	code: 200,
 	data: ''
 }
 
-router.get('*', (ctx) => {
-	const html = fs.readFileSync(resolve('../../../dist/' + 'index.html'), 'utf-8')
-	ctx.body = html
-})
+/**
+ * 笔记 C、R、U、D
+ */
 
 router.post('/api/generateNote', async (ctx, next) => {
 	await new Promise((resolve, reject) => {
@@ -48,6 +48,10 @@ router.post('/api/addNote', async (ctx, next) => {
 	})
 })
 
+/**
+ * 标签 C、R、U、D
+ */
+
 router.post('/api/addTag', async (ctx, next) => {
 	await new Models.TagList(ctx.request.body).save((err, docs) => {
 		if(err){
@@ -59,8 +63,31 @@ router.post('/api/addTag', async (ctx, next) => {
 	})
 })
 
-// router.get('/test', (ctx) => {
-//   ctx.body = 'testwwww!';
-// })
+router.post('/api/deleteTag', async (ctx, next) => {
+	await Models.TagList.remove({_id: ctx.request.body.id}, (err, docs) => {
+		if(err){
+			ctx.throw(500)
+			return
+		}
+		successState.data = docs
+		ctx.response.body = successState
+	})
+})
+
+router.get('/api/TagList', async (ctx, next) => {
+	await Models.TagList.find({}, (err, docs) => {
+		if(err){
+			ctx.throw(500)
+			return
+		}
+		successState.data = docs
+		ctx.response.body = successState
+	})
+})
+
+router.get('*', (ctx) => {
+	const html = fs.readFileSync(resolve('../../../dist/' + 'index.html'), 'utf-8')
+	ctx.body = html
+})
 
 module.exports = router
